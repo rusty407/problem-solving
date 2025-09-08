10a641cdbfa6cff62c33463a24ca361125b8f7be
rusty407 <sandroamiridze93@gmail.com>
2025-09-08 13:17:45 -0400

    Time: 0 ms (100.00%) | Memory: 9.3 MB (68.44%) - LeetSync

---
+++ 792-binary-search/binary-search.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// int binary_search(int a[], int e, int l, int r);
+int search(int* nums, int numsSize, int target) {
+    int l = 0;
+    int r = numsSize - 1;
+while (l <= r ) { 
+        int mid = l + (r - l) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return -1;
+}
